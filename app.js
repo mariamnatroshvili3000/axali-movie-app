7c4d7fe072296e5ce71bf4e890f029b205972c55
mariamnatroshvili3000 <112975366+mariamnatroshvili3000@users.noreply.github.com>
2022-11-29 03:31:08 +0400

    azrze ara var
    azrze ver movdivar

---
+++ app.js
@@ -0,0 +1,16 @@
+'use srict'
+// navbar variables
+const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+const navCloseBtn= document.querySelector("[data-menu-close-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay= document.querySelector("[data-overlay]")
+
+const navElemArr =[navOpenBtn, navCloseBtn, overlay];
+
+for(let i = 0; i < navElemArr.length; i++) {
+    naveElemArr[i].addEventListener("click", function(){
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+        document.body.classList.toggle("active");
+    })
+}
